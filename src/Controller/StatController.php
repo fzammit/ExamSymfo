b0e114710bf292cbd98e1ca7bb67d0d7503671c7
Fzammit <frederic.zammit@hotmail.fr>
2020-03-20 12:28:45 +0100

    stat form made :godmode:

--- src/Controller/StatController.php
+++ src/Controller/StatController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Stat;
+use App\Form\StatType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -34,6 +35,13 @@ class StatController extends AbstractController
      */
     public function new()
     {
+
+        $stat = new Stat();
+        $form = $this->createForm(StatType::class, $stat);
+
+        return $this->render('stat/form.html.twig', [
+            'form' => $form->createView()
+        ]);
     }
 
     /**
